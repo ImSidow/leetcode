1573106fc8288b6b499f5fe62a16dec9ce09098b
Asad Ali Sidow <sidow.code@gmail.com>
2022-07-26 16:38:24 +0300

    fizz buzz bug fixed

--- fizzBuzz/index.js
+++ fizzBuzz/index.js
@@ -1,12 +1,12 @@
 function fizzBuzz(n) {
     var items = [];
-    for (var i = 0; i < n; i++) {
-        if (i % 3 == 0)
+    for (var i = 1; i < n + 1; i++) {
+        if (i % 3 == 0 && i % 5 == 0)
+            items.push("FizzBuzz");
+        else if (i % 3 == 0)
             items.push("Fizz");
         else if (i % 5 == 0)
             items.push("Buzz");
-        else if (i % 3 == 0 && i % 5 == 0)
-            items.push("FizzBuzz");
         else
             items.push("".concat(i));
     }
--- fizzBuzz/index.ts
+++ fizzBuzz/index.ts
@@ -1,12 +1,12 @@
 function fizzBuzz(n: number): string[] {
     let items = [];
-    for (let i = 0; i < n; i++) {
-        if (i % 3 == 0) items.push("Fizz");
+    for (let i = 1; i < n + 1; i++) {
+        if (i % 3 == 0 && i % 5 == 0) items.push("FizzBuzz");
+        else if (i % 3 == 0) items.push("Fizz");
         else if (i % 5 == 0) items.push("Buzz");
-        else if (i % 3 == 0 && i % 5 == 0) items.push("FizzBuzz");
         else items.push(`${i}`);
     }
     return items;
 }
 
-console.log(fizzBuzz(5))
+console.log(fizzBuzz(5));
